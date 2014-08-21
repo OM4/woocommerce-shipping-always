1b28ab28da9e80a2f18163257f84d91aa2ddb1ba
James Collins <james@om4.com.au>
2014-08-21 09:56:33 +0800

    Initial Commit. Bugz 2661

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== WooCommerce Post Classes ===
+Contributors: jamescollins
+Tags: woocommerce, shipping, checkout, virtual, address
+Requires at least: 3.9
+Tested up to: 3.9.2
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+During checkout, always ask the customer for a shipping address. Useful if you have virtual products that still need a shipping address.
+
+== Description ==
+
+In WooCommerce, if a customer orders only virtual products from your store, they aren't asked for a shipping address during checkout.
+
+This plugin ensures that the customer must enter a shipping address.
+
+Useful if you have virtual products that still need a shipping address.
+
+Also useful if you have products where their shipping cost is included in their price. For these products, set them as virtual products, and activate this plugin.
+
+If the customer buys only virtual products, they will be able to check out without selecting a shipping method.
+If the customer buys a combination of virtual and physical products, their shipping costs will only include the physical products (ie. the virtual products are excluded from the shipping cost calculations).
+
+If you have any other suggestions for other information that could be included, please [add your suggestion via GitHub](https://github.com/OM4/woocommerce-shipping-always/issues).
+
+== Installation ==
+
+1. Download the latest stable version of the plugin from https://github.com/OM4/woocommerce-shipping-always/archive/release.zip.
+1. Install this plugin via WordPress Dashboard, Plugins, Add New, Upload.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release.
---
+++ woocommerce-shipping-always.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: WooCommerce - Always Ask for Shipping Address
+Plugin URI: https://github.com/OM4/woocommerce-shipping-always
+Description: During checkout, always ask the customer for a shipping address. Useful if you have virtual products that still need a shipping address.
+Version: 0.1
+Author: OM4
+Author URI: http://om4.com.au/
+Text Domain: woocommerce-shipping-always
+Git URI: https://github.com/OM4/woocommerce-shipping-always
+Git Branch: release
+License: GPLv2
+*/
+
+/*
+Copyright 2014 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! class_exists( 'WC_Shipping_Always' ) ) {
+
+	/**
+	 * This class is a singleton.
+	 *
+	 * Class WC_Shipping_Always
+	 */
+	class WC_Shipping_Always {
+
+		/**
+		 * Refers to a single instance of this class
+		 */
+		private static $instance = null;
+
+		/**
+		 * Creates or returns an instance of this class
+		 * @return WC_Shipping_Always A single instance of this class
+		 */
+		public static function instance() {
+			if ( null == self::$instance ) {
+				self::$instance = new self();
+			}
+			return self::$instance;
+
+		}
+
+		/**
+		 * Constructor
+		 */
+		private function __construct() {
+			add_filter( 'woocommerce_cart_needs_shipping_address', '__return_true' );
+		}
+
+	}
+
+	WC_Shipping_Always::instance();
+
+}
